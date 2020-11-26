db46d93756323a6494441a5965ce43cde0fbc6e0
Elona Bayley <28581@ma-web.nl>
2020-11-26 10:39:30 +0100

    removed comments

--- script1.js
+++ script1.js
@@ -5,28 +5,24 @@
     let aftrekken = document.getElementById('aftrekken');
     let delen = document.getElementById('delen');
     mijnButton.addEventListener('click', function(){
-        // lees de waarden van de text input velden in 
-        // let op DIT ZIJN STRINGS
         let getal1 = document.getElementById('getal1').value;        
-        let getal2 = document.getElementById('getal2').value;      
-        // zet de ingelezen strings om in getallen
+        let getal2 = document.getElementById('getal2').value;   
         getal1 = parseFloat(getal1);
         getal2 = parseFloat(getal2);
-        // roep de function aan met arguments
         telOp(getal1,getal2);
         vermenigvuldig(getal1,getal2);
         trekAf(getal1,getal2);
         deel(getal1,getal2);
     })
     function telOp(getal1, getal2){
-        let antwoord = getal1 + getal2;// doe de bewerking
-        console.log("optellen " + antwoord); // laat ook in de console zien
-        optellen.innerHTML = antwoord; // schrijf naar HTML
+        let antwoord = getal1 + getal2;
+        console.log("optellen " + antwoord);
+        optellen.innerHTML = antwoord;
     }
     function vermenigvuldig(getal1, getal2){
-        let antwoord = getal1 * getal2;// doe de bewerking
-        console.log("vermenigvuldigen " + antwoord); // laat ook in de console zien
-        vermenigvuldigen.innerHTML = antwoord; // schrijf naar HTML
+        let antwoord = getal1 * getal2;
+        console.log("vermenigvuldigen " + antwoord);
+        vermenigvuldigen.innerHTML = antwoord;
     }
     function trekAf(getal1, getal2){
         let antwoord = getal1 - getal2;
